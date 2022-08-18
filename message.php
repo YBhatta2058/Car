ab206d63b8431f7e6aa9368c3e63fd640384b30a
YBhatta2058 <ybhatta70@gmail.com>
2022-08-18 10:28:38 +0530

    Hi

---
+++ message.php
@@ -0,0 +1,30 @@
+<?php
+echo "Patient Connection successful...";
+$PEMAIL = $_POST['email'];
+$PNAME=$_POST['name'];
+$message=$_POST['msg'];
+
+
+// Database connection
+$conn= new mysqli ('localhost','root','','shristi');
+if($conn->connect_error){
+    die('Connection Failed:'.$conn->connect_error);
+
+}
+else{
+    // $stmt =$conn->prepare("insert into patient(PID,PNAME,PAGE,PDIAGNOSED,PDID,PSTATUS) values(?,?,?,?,?,?)");
+    // $stmt->bind_param("isssis",$PID,$PNAME,$PAGE,$PDIGNOSED,$PDID,$PSTATUS);
+    $sql = "INSERT INTO Message (Name,Email,Message)
+VALUES ('$PNAME','$PEMAIL','$message')";
+
+
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+};
+?>
